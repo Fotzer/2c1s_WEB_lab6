6a04bd788aacc14cf34aede13608a2e37ee118cf
Maxik <mpoplavskiy38380@gmail.com>
2023-11-17 16:16:03 +0200

    Add 404 if file not exists

--- readfile.php
+++ readfile.php
@@ -1,3 +1,10 @@
 <?php
-  readfile($_GET["objectname"]);
+    if (file_exists($_GET["objectname"]))
+    {
+        readfile($_GET["objectname"]);
+    }
+    else
+    {
+        header('HTTP/1.0 404 not found');
+    }
 ?>
